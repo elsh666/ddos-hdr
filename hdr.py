686ccb8aaaac861304089f69468ff8d168ca8010
elsh666 <105549104+elsh666@users.noreply.github.com>
2022-05-30 23:51:40 +0300

    Update hdr.py

--- hdr.py
+++ hdr.py
@@ -757,9 +757,7 @@ class RequestDefaultHTTP(threading.Thread): # la classe del multithreading
 				s.connect((str(url2), int(urlport))) # connessione
 				s.send (str.encode(request)) # invio
 				print ("Request sent! @") # print req + counter
-				try: # invia altre richieste nello stesso thread
-					for y in range(multiple): # fattore di moltiplicazione
-						s.send(str.encode(request)) # encode in bytes della richiesta HTTP
+				
 				except: # se qualcosa va storto, chiude il socket e il ciclo ricomincia
 					s.close()
 			except: # se qualcosa va storto
